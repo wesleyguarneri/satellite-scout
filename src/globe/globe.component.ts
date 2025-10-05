9978ea5dfe19dd5fbce918c6685d3c257f8a710c
wlg223 <wesguarneri@gmail.com>
2025-10-05 08:24:05 -0400

    updating angular cli

--- src/globe/globe.component.ts
+++ src/globe/globe.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-globe',
-  standalone: true,
-  imports: [],
-  templateUrl: './globe.component.html',
-  styleUrl: './globe.component.css'
+    selector: 'app-globe',
+    imports: [],
+    templateUrl: './globe.component.html',
+    styleUrl: './globe.component.css'
 })
 export class GlobeComponent {
 
