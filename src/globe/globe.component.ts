2fdc4b91227e89ea77c670da8879af0b373ded84
wlg223 <wesguarneri@gmail.com>
2025-10-04 20:51:48 -0400

    adding globe component

---
+++ src/globe/globe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-globe',
+  standalone: true,
+  imports: [],
+  templateUrl: './globe.component.html',
+  styleUrl: './globe.component.css'
+})
+export class GlobeComponent {
+
+}
