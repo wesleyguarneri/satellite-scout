3cd57bc0a72ddc06fdc7d3a417aefb51635b2b7e
wlg223 <wesguarneri@gmail.com>
2025-10-09 12:20:14 -0400

    removing node models

--- src/app/models/nodes/node-api-response.ts
+++
@@ -1,8 +0,0 @@
-import { NamedValue } from "../named-value";
-
-export type NodeApiResponse = {
-  processed: boolean;
-  status: string;
-  timestamp: number;
-  additionalAttributes: NamedValue[]
-}
